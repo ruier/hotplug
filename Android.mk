ee7f8f2454672ea4ed19b1718d060d24ca9f2d9a
Royce Jiang <jiangxd@embest-tech.com>
2015-12-21 10:34:24 +0800

    add the source code of the hotplug and Android.mk for build
    Signed-off-by: Royce Jiang <jiangxd@embest-tech.com>

---
+++ Android.mk
@@ -0,0 +1,23 @@
+LOCAL_PATH:= $(call my-dir)
+ 
+include $(CLEAR_VARS)
+ 
+LOCAL_SRC_FILES := hotplug.c                             
+ 
+LOCAL_MODULE:= hotplug
+ 
+LOCAL_C_INCLUDES := $(KERNEL_HEADERS) \
+                    external/mdnsresponder/mDNSShared \
+                    external/openssl/include \
+                    external/stlport/stlport \
+                    bionic \
+                    bionic/libc/private \
+                    $(call include-path-for, libhardware_legacy)/hardware_legacy
+ 
+LOCAL_CFLAGS := -Werror=format
+ 
+LOCAL_SHARED_LIBRARIES := libstlport libsysutils libcutils libnetutils \
+                          libcrypto libhardware_legacy libmdnssd libdl
+ 
+include $(BUILD_EXECUTABLE)
+
